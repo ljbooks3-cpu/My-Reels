a78e056fae8f29e6bb0a3a73bdce32fd14c26200
ljbooks3-cpu <ljbooks3@gmail.com>
2026-08-16 12:34:30 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='my-reels-v22-1-drivefetch';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{if(r.ok)caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match(e.request)));
+  }
+});
