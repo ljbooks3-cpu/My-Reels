5c40c3a3b493995a596a0859a4c8dc5c963901bc
ljbooks3-cpu <ljbooks3@gmail.com>
2026-08-16 22:16:34 +0530

    Connect public viewer to Apps Script catalogue

--- public-config.js
+++ public-config.js
@@ -1,2 +1,2 @@
-window.MY_REELS_PUBLIC_API='__APPS_SCRIPT_URL__';
+window.MY_REELS_PUBLIC_API='https://script.google.com/macros/s/AKfycbyIcTsXxu896spK_ZSZnyciyw-XMw6_JLPx9oAjJugVdnUnonYqN5OrJvcT1MNY5ZI9/exec';
 window.MY_REELS_CREATOR_EMAIL='ljbooks3@gmail.com';
