1abe268399da43b78b42a0d63c444386228f51b1
ljbooks3-cpu <ljbooks3@gmail.com>
2026-08-16 21:52:19 +0530

    Add public viewer bootstrap

---
+++ public-manifest.js
@@ -0,0 +1,6 @@
+(()=>{'use strict';
+/* Public viewer bootstrap. This intentionally contains no Google OAuth. The public
+   catalogue is populated by the creator-published manifest when available. */
+const MANIFEST_URL='';
+window.MyReelsPublic={manifestUrl:MANIFEST_URL};
+})();
